1e8700e0ee1036b78b9c1e21619dc52bcebdcbea
TakuyaKawabe <rapidarrow2000@gmail.com>
2018-06-22 15:34:42 +0900

    ハミング距離算出機能追加

---
+++ ImageHash/ImageHash.Test/ProgramTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ImageHash.Test
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void ハミング距離テスト_黒と黒_同一ファイル()
+        {
+            var hammingDistance = ImageHash.Program.GetHammingDistance(
+                "./TestFiles/SampleImages/image_black.jpg",
+                "./TestFiles/SampleImages/image_black.jpg");
+
+            hammingDistance.Is(0);
+
+        }
+
+
+        [TestMethod]
+        public void ハミング距離テスト_同一ファイル()
+        {
+            var hammingDistance = ImageHash.Program.GetHammingDistance(
+                "./TestFiles/SampleImages/image_1.jpg",
+                "./TestFiles/SampleImages/image_1.jpg");
+
+            hammingDistance.Is(0);
+        }
+
+
+        [TestMethod]
+        public void ハミング距離テスト_黒と白()
+        {
+            var hammingDistance = ImageHash.Program.GetHammingDistance(
+                "./TestFiles/SampleImages/image_black.jpg",
+                "./TestFiles/SampleImages/image_white.jpg");
+
+            hammingDistance.Is(8);
+
+        }
+
+        [TestMethod]
+        public void ハミング距離テスト_1と2()
+        {
+            var hammingDistance = ImageHash.Program.GetHammingDistance(
+                "./TestFiles/SampleImages/image_1.jpg",
+                "./TestFiles/SampleImages/image_2.jpg");
+
+            hammingDistance.Is(31);
+        }
+
+        [TestMethod]
+        public void ハミング距離テスト_1と3()
+        {
+            var hammingDistance = ImageHash.Program.GetHammingDistance(
+                "./TestFiles/SampleImages/image_1.jpg",
+                "./TestFiles/SampleImages/image_3.jpg");
+
+            hammingDistance.Is(2);
+        }
+
+        [TestMethod]
+        public void ハミング距離テスト_4と5()
+        {
+            var hammingDistance = ImageHash.Program.GetHammingDistance(
+                "./TestFiles/SampleImages/image_4.jpg",
+                "./TestFiles/SampleImages/image_5.jpg");
+
+            hammingDistance.Is(32);
+        }
+        [TestMethod]
+        public void ハミング距離テスト_4と6()
+        {
+            var hammingDistance = ImageHash.Program.GetHammingDistance(
+                "./TestFiles/SampleImages/image_4.jpg",
+                "./TestFiles/SampleImages/image_6.jpg");
+
+            hammingDistance.Is(31);
+        }
+    }
+}
--- ImageHash/ImageHash/Program.cs
+++ ImageHash/ImageHash/Program.cs
@@ -1,15 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ImageHash
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+        }
+
+
+        public static int GetHammingDistance(string path1, string path2)
+        {
+            var img1 = File.GetImage(path1);
+            var img2 = File.GetImage(path2);
+
+            return GetHammingDistance(img1, img2);
+        }
+
+
+        public static int GetHammingDistance(System.Drawing.Image img1, System.Drawing.Image img2)
+        {
+            var img1Hash = GetImgHash(img1);
+            var img2Hash = GetImgHash(img2);
+
+            if (img1Hash.Length != img2Hash.Length)
+                throw new Exception("Hash size is different");
+
+            int unmatchCount = 0;
+            for (int i = 0; i < img1Hash.Length; i++)
+            {
+                if (img1Hash[i] != img2Hash[i])
+                    unmatchCount++;
+            }
+
+            return unmatchCount;
+        }
+
+
+        public static byte[] GetImgHash(System.Drawing.Image img)
+        {
+            var imgData = new Image(img);
+            imgData.Resize(new Size(9, 8));
+            imgData.ChangeGrayscale();
+
+            return imgData.GetDHash();
         }
     }
 }
